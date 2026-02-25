6f4df76d77449c1c15019f72dd40274c046975a4
vadim <bockarevvadim98@gmail.com>
2026-02-25 16:45:12 +0300

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Main from './pages/main page/main.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Main />
   </StrictMode>,
 )
---
+++ src/pages/main page/main.tsx
@@ -0,0 +1,14 @@
+import './main.css'
+
+function Main() {
+
+
+  return (
+    <div>
+      <nav>
+
+      </nav>
+    </div>
+  )
+}
+export default Main
