30d64288fced5a123461fda9b07c6afcaf3d1d9c
vadim <bockarevvadim98@gmail.com>
2026-03-11 22:38:42 +0300

    add delite project

--- src/pages/main page/main.tsx
+++ src/pages/main page/main.tsx
@@ -35,14 +35,36 @@ function Main() {
             </MenuContext.Provider>
 
             {projects ? projects.map(project => (
+              <>
+              <div className="cardColision" style={{width: '200px', height: '150px'}}>
               <div 
-                className='card'
+                className={`card ${expendedCardId === project.id ? 'activeCard' : ''}`}
                 key={project.id}
                 onClick={() => setExpendedCardId(prev => prev === project.id ? null : project.id)}
               >
                 {expendedCardId === project.id ? (
                   <>
-                    <h2>{project.name}</h2>
+                    <span className="close-icon">×</span>
+
+                    <h3 >{project.name}</h3>
+                    <p>{project.description}</p>
+                    <div className="card-actions">
+                      <button 
+                        className='rename'
+                        onClick={(e) => { e.stopPropagation(); /* тут твоя логика кнопки */ }}>
+                          Rename
+                      </button>
+                      <button
+                        className='delete'
+                        onClick={(e) => { e.stopPropagation(); projectsStore.getState().delete(project.id) }}>
+                        Delete
+                      </button>
+                      <button 
+                        className='open'
+                        onClick={(e) => { e.stopPropagation(); /* тут твоя логика кнопки */ }}>
+                        Open
+                      </button>
+                    </div>
                   </>
                 ) : (
                   <>
@@ -51,6 +73,8 @@ function Main() {
                   </>
                 )}
               </div>
+              </div>
+            </>
             )) : <p>No projects found</p>}
           </div>
         </div>
